67fd92d1dc9156dfac6d79c2e1a3b795c2c71c63
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2020-11-27 10:20:13 +0900

    サンプル追加

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_core_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
